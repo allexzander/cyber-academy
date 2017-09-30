779ac29a779eff8ead495cf8e245f88b3e39936a
Alexande Zolotov <slayer666@debian-slayer666.alexander.zolotov.net>
2017-09-30 17:43:47 +0300

    added support for localized stings in Faculties component

--- src/routes/Faculties/components/MainView.js
+++ src/routes/Faculties/components/MainView.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import firebase from 'firebase'
+import LocalizedStrings from 'react-localization'
 import {browserHistory} from 'react-router'
 import './faculties.scss'
 class MainView extends Component {
@@ -8,13 +9,95 @@ class MainView extends Component {
 
     this.state = {
       courses: [],
-      coursesFetched: false
+      coursesFetched: false,
+      siteInfoLoaded: false,
+      words: ['CSGOButton', 'CSGODescription', 'DotaButton', 'DotaDescription', 'LoLButton', 'LoLDescription'],
+      wordsEng: {},
+      wordsRu: {},
+      localizedStrings: {},
+      language: "RU",
+      DotaButton: '',
+      LoLButton: '',
+      CSGOButton: '',
+      DotaDescription: '',
+      LoLDescription: '',
+      CSGODescription: '',
+      DotaImg: '',
+      LoLImg: '',
+      CSGOImg: ''
     }
   }
+
+  /*TODO: move localization code to one single place*/
+  makeStrings () {
+    const { words, wordsEng = {}, wordsRu = {} } = this.state
+    let rus = {}
+    let eng = {}
+    words.forEach(item => {
+      eng[`${item}`] = wordsEng !== {} ? wordsEng[`${item}Eng`] : 'no data'
+      rus[`${item}`] = wordsRu !== {} ? wordsRu[`${item}Ru`] : 'no data'
+    })
+    let localizedStrings = new LocalizedStrings({
+      rus: rus,
+      eng: eng
+    })
+    this.setState({ localizedStrings })
+  }
+
+  saveInfo (language, suff, object) {
+    const { words } = this.state
+    if (suff === 'Ru') {
+      let wordsRu = {}
+      words.forEach(item => {
+        wordsRu[`${item}${suff}`] = object[`${language}`][`${item}${suff}`]
+      })
+      this.setState({ wordsRu, siteInfoLoaded: true })
+    } else if (suff === 'Eng') {
+      let wordsEng = {}
+      words.forEach(item => {
+        wordsEng[`${item}${suff}`] = object[`${language}`][`${item}${suff}`]
+      })
+      this.setState({ wordsEng, siteInfoLoaded: true })
+    }
+  }
+
+  setLanguage (language) {
+    const { localizedStrings } = this.state
+    console.log('set ', language)
+    localizedStrings.setLanguage(language.language)
+    this.setState({ localizedStrings })
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log("HomeView componentWillReceiveProps");
+    this.props.language !== nextProps.language && this.setLanguage(nextProps.language)
+  }
+
+  componentDidMount () {
+    this.fetchText('faculties')
+  }
+
+  fetchText (page) {
+    firebase.database().ref('siteInfo/' + `${page}/`)
+    .once('value')
+    .then(snapshot => {
+      const object = snapshot.val()
+      if (object !== null) {
+        this.saveInfo('russian', 'Ru', object)
+        this.saveInfo('english', 'Eng', object)
+        this.makeStrings()
+      } else {
+        this.setState({ siteInfoLoaded: true })
+      }
+    })
+  }
+  /*-----------------------------------------------------------------*/
 
   componentWillMount() {}
 
   render() {
+    const { localizedStrings } = this.state;
+
     return (
       <div className='container'>
         <div className='content-faculties'>
@@ -28,12 +111,12 @@ class MainView extends Component {
               }} onClick={() => {
                 browserHistory.push({pathname: '/faculty/CS:GO'})
               }}>
-                Поступить на факультет {/* {CSGOButton} */}
+                {localizedStrings.CSGOButton}
               </div>
               <div className='textFrame-faculties' style={{
                 backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/textFrame.jpg?alt=media&token=fc8f06c4-b584-4303-9a81-e3ded24b2a10)'
               }}>
-                Получай заслуженные награды {/* {CSGODescription} */}
+              {localizedStrings.CSGODescription}
               </div>
             </div>
             <div className='col-sm-4 col-md-4'>
@@ -45,12 +128,12 @@ class MainView extends Component {
               }} onClick={() => {
                 browserHistory.push({pathname: '/faculty/Dota2'})
               }}>
-                Поступить на факультет {/* {DotaButton} */}
+                {localizedStrings.DotaButton}
               </div>
               <div className='textFrame-faculties' style={{
                 backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/textFrame.jpg?alt=media&token=fc8f06c4-b584-4303-9a81-e3ded24b2a10)'
               }}>
-                Получай заслуженные награды {/* {DotaDescription} */}
+                {localizedStrings.DotaDescription}
               </div>
             </div>
             <div className='col-sm-4 col-md-4'>
@@ -62,12 +145,12 @@ class MainView extends Component {
               }} onClick={() => {
                 browserHistory.push({pathname: '/faculty/LoL'})
               }}>
-                Поступить на факультет {/* {LoLButton} */}
+                {localizedStrings.LoLButton}
               </div>
               <div className='textFrame-faculties' style={{
                 backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/textFrame.jpg?alt=media&token=fc8f06c4-b584-4303-9a81-e3ded24b2a10)'
               }}>
-                Получай заслуженные награды {/* {LoLDescription} */}
+                {localizedStrings.LoLDescription}
               </div>
             </div>
           </div>
