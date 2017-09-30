4a6440eab12d1fa1faa977f6229c7c42b7521824
Alexande Zolotov <slayer666@debian-slayer666.alexander.zolotov.net>
2017-09-30 16:22:49 +0300

    added localization change support for HomeView

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,7 +22,7 @@ class Header extends Component {
     console.log("Language change")
   }
 
-  componentDidMount () {
+  componentWillMount () {
     this.fetchText('header')
   }
 
@@ -33,6 +33,7 @@ class Header extends Component {
   setLanguage (language) {
     const { strings } = this.state
     console.log('set ', language)
+    console.log(strings);
     strings.setLanguage(language.language)
     this.setState({ strings })
   }
@@ -91,77 +92,6 @@ class Header extends Component {
     console.log(strings.logOut)
     const classNameFirstButton = Boolean(Object.keys(user).length) ?
     'dropdown firstButtonLogin' : 'dropdown firstButtonLogout'
-    /*return (
-      <div>
-        <div className='col-xs-12 col-md-12'>
-          <div
-            style={{ float: 'right'}}
-            onClick={() => {
-              onEngLang()
-            }
-          }
-          >  ENG
-        </div>
-        <div
-          style={{ float: 'right'}}
-          onClick={() => {
-            onRusLang()
-          }
-        }
-        >RUS|
-      </div>
-    </div>
-    <div className='container'>
-      <div>
-        <a href='/' title='Return to the homepage'>
-        <div className='logo'
-          style={{ backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/logo.png?alt=media&token=338a7d4a-1e8a-4b6a-87a0-0d9e2e9353dc)'}}
-          ></div>
-        </a>
-      </div>
-      <div className={classNameFirstButton}>
-        <div className='navbtn1'
-          onClick={() => { browserHistory.push({ pathname: `/faculties` }) }}
-          >{strings.faculties}</div>
-          <div className='dropdown-content firstButton-dropdown'>
-            <a href='/faculty/CS:GO'>CS:GO</a>
-            <a href='/faculty/Dota2'>Dota2</a>
-            <a href='/faculty/LoL'>LOL</a>
-          </div>
-        </div>
-        <div
-          className='navbtn2'
-          onClick={() => { browserHistory.push({ pathname: `` }) }}
-          >{strings.tournaments}
-        </div>
-        <div
-          className='navbtn2'
-          onClick={() => { browserHistory.push({ pathname: '/forum' }) }}
-          >{strings.forum}
-        </div>
-        {!Object.keys(user).length &&
-          <div
-            className='navbtnLogin'>
-
-            <a href={`${backend}/auth/steam`}> LOGIN </a>
-          </div> }
-
-          {Boolean(Object.keys(user).length) &&
-            <div className='dropdown'>
-              <div className='navbtn4'>{user.displayName}</div>
-              <div className='dropdown-content'>
-                <a href='/MyCourses'>{strings.myCourses}</a>
-                <a href='/statistics'>{strings.statistics}</a>
-                <a href='/'
-                onClick={() => firebase.auth().signOut()}>
-                Log out</a>
-              </div>
-            </div>
-          }
-        </div>
-      </div>
-    )*/
-//<li><a className="btn btn-default button_shadow-golden button-golden" href={`${backend}/auth/steam`}> LOGIN </a></li>
     return (<div className="row">
     <div className="col-sm-12">
         <div id="header">
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -4,6 +4,7 @@ import './HomeViewNew.scss'
 import Slider from 'react-slick'
 import VideoPlayer from './VideoPlayer'
 import firebase from 'firebase'
+import LocalizedStrings from 'react-localization'
 
 class HomeView extends Component {
   constructor (props) {
@@ -13,53 +14,84 @@ class HomeView extends Component {
       transactionToChange: {},
       stopVideo:false,
       siteInfoLoaded: false,
-      quaterText1Ru: '',
-      quaterText2Ru: '',
-      quaterText3Ru: '',
-      quaterText4Ru: '',
-      linkVideoToParentsRu: '',
-      buttonTextRu: '',
-      videoButtonVideoToParentsRu: '',
-      videoButtonCoverVideoRu: ''
+      words: ['buttonText', 'linkVideoToParents', 'quaterText1', 'quaterText2', 'quaterText3', 'quaterText4',
+       'videoButtonCoverVideo', 'videoButtonVideoToParents'],
+      wordsEng: {},
+      wordsRu: {},
+      localizedStrings: {},
+      language: "RU"
     }
   }
 
-  componentWillMount () {
-    firebase.database().ref('siteInfo/' + 'russian/' + 'homepage')
+  /*TODO: move localization code to one single place*/
+  makeStrings () {
+    const { words, wordsEng = {}, wordsRu = {} } = this.state
+    let rus = {}
+    let eng = {}
+    words.forEach(item => {
+      eng[`${item}`] = wordsEng !== {} ? wordsEng[`${item}Eng`] : 'no data'
+      rus[`${item}`] = wordsRu !== {} ? wordsRu[`${item}Ru`] : 'no data'
+    })
+    let localizedStrings = new LocalizedStrings({
+      rus: rus,
+      eng: eng
+    })
+    this.setState({ localizedStrings })
+  }
+
+  saveInfo (language, suff, object) {
+    const { words } = this.state
+    if (suff === 'Ru') {
+      let wordsRu = {}
+      words.forEach(item => {
+        wordsRu[`${item}${suff}`] = object[`${language}`][`${item}${suff}`]
+      })
+      this.setState({ wordsRu, siteInfoLoaded: true })
+    } else if (suff === 'Eng') {
+      let wordsEng = {}
+      words.forEach(item => {
+        wordsEng[`${item}${suff}`] = object[`${language}`][`${item}${suff}`]
+      })
+      this.setState({ wordsEng, siteInfoLoaded: true })
+    }
+  }
+
+  setLanguage (language) {
+    const { localizedStrings } = this.state
+    console.log('set ', language)
+    localizedStrings.setLanguage(language.language)
+    this.setState({ localizedStrings })
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.props.language !== nextProps.language && this.setLanguage(nextProps.language)
+  }
+
+  componentDidMount () {
+    this.fetchText('homepage')
+  }
+
+  fetchText (page) {
+    firebase.database().ref('siteInfo/' + `${page}/`)
     .once('value')
     .then(snapshot => {
       const object = snapshot.val()
       if (object !== null) {
-        const {
-          quaterText1Ru,
-          quaterText2Ru,
-          quaterText3Ru,
-          quaterText4Ru,
-          linkVideoToParentsRu,
-          buttonTextRu,
-          videoButtonVideoToParentsRu,
-          videoButtonCoverVideoRu
-        } = object
-        this.setState({
-          quaterText1Ru,
-          quaterText2Ru,
-          quaterText3Ru,
-          quaterText4Ru,
-          linkVideoToParentsRu,
-          buttonTextRu,
-          videoButtonVideoToParentsRu,
-          videoButtonCoverVideoRu,
-          siteInfoLoaded: true })
+        this.saveInfo('russian', 'Ru', object)
+        this.saveInfo('english', 'Eng', object)
+        this.makeStrings()
       } else {
         this.setState({ siteInfoLoaded: true })
       }
     })
   }
-
+  /*-------------------------------------------------------*/
+  
   render () {
+    const { localizedStrings } = this.state;
     let videoUrl = ""; //linkVideoToParentsRu
     const { showVideo, stopVideo } = this.state
-    console.log(this.props)
+
     const settings = {
       accessibility: false,
       autoplay: true,
@@ -73,18 +105,8 @@ class HomeView extends Component {
       slidesToShow: 1,
       slidesToScroll: 1
     }
-    const {
-      quaterText1Ru,
-      quaterText2Ru,
-      quaterText3Ru,
-      quaterText4Ru,
-      linkVideoToParentsRu,
-      buttonTextRu,
-      videoButtonVideoToParentsRu,
-      videoButtonCoverVideoRu
-    } = this.state
     const classNameButtonPause = stopVideo && showVideo ? 'pauseVideoToParents-home' : 'playVideoToParents-home'
-    const videoButtonName = showVideo ? videoButtonCoverVideoRu : videoButtonVideoToParentsRu
+    const videoButtonName = showVideo ? localizedStrings.videoButtonCoverVideo : localizedStrings.videoButtonVideoToParents;
     return (
       <span className="no-any-class">
           {!showVideo &&
@@ -98,23 +120,23 @@ class HomeView extends Component {
     <div className="row" id="content">
         <div className="col-sm-6 col-md-4 col-lg-3">
             <div className="section"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/circle_inner_ornament.png?alt=media&token=e1ccd349-0525-486a-b311-9e90448f89aa" alt="circle_ornament"/>
-                <div className="description text-uppercase">{quaterText1Ru}</div>
+                <div className="description text-uppercase">{localizedStrings.quaterText1}</div>
                 <div className="circle"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/gears.png?alt=media&token=d6509832-ff0d-4eec-8267-46cf02344579" alt="icon_gears"/></div></div>
             </div>
             <div className="col-sm-6 col-md-4 col-lg-3">
                 <div className="section"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/circle_inner_ornament.png?alt=media&token=e1ccd349-0525-486a-b311-9e90448f89aa" alt="circle_ornament"/>
-                    <div className="description text-uppercase">{quaterText2Ru}</div>
+                    <div className="description text-uppercase">{localizedStrings.quaterText2}</div>
                     <div className="circle"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/book.png?alt=media&token=c5aaa8b8-1172-40cc-9369-5446700ae174" alt="icon_book"/></div></div>
             </div>
 			<div className="clearfix visible-sm-block"></div>
             <div className="col-sm-6 col-md-4 col-lg-3">
                 <div className="section"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/circle_inner_ornament.png?alt=media&token=e1ccd349-0525-486a-b311-9e90448f89aa" alt="circle_ornament"/>
-                    <div className="description text-uppercase">{quaterText3Ru}</div>
+                    <div className="description text-uppercase">{localizedStrings.quaterText3}</div>
                     <div className="circle"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/hat.png?alt=media&token=e106cf0e-3dff-4276-9fc6-bb30c5584a92" alt="icon_hat"/></div></div>
             </div>
             <div className="col-sm-6 col-md-12 col-lg-3">
                 <div className="section"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/circle_inner_ornament.png?alt=media&token=e1ccd349-0525-486a-b311-9e90448f89aa" alt="circle_ornament"/>
-                    <div className="description text-uppercase">{quaterText4Ru}</div>
+                    <div className="description text-uppercase">{localizedStrings.quaterText4}</div>
                     <div className="circle"><img src="https://firebasestorage.googleapis.com/v0/b/cyber-academy.appspot.com/o/trophy.png?alt=media&token=4e251814-85d7-4674-b19d-72ecbc8e30d3" alt="icon_trophy"/></div></div>
             </div>
         </div>
@@ -122,7 +144,7 @@ class HomeView extends Component {
             <div className="col-sm-12">
                 <div className="text-center" id="start_course">
                   <button type="button" className="btn btn-default button_shadow-golden button-golden" onClick={() => { browserHistory.push({ pathname: '/faculties' }) }}>
-                    {buttonTextRu}</button></div>
+                    {localizedStrings.buttonText}</button></div>
             </div>
         </div>
         </span>
