aac7cd5841d7c865959057b18b30d6eadb4114b5
Alexande Zolotov <slayer666@debian-slayer666.alexander.zolotov.net>
2017-10-07 22:37:25 +0300

    refactoring

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,6 +2,36 @@ const OpenDotaUtils = require('../utils/opendota.js');
 const FirbaseUtils = require('../utils/firebase.js');
 const Routes = require('express').Router();
 
+let formatOpenDotaStatistics = function(openDotaStats) {
+    let statistics = {matches : []};
+
+    let matches = openDotaStats.matches;
+    
+    for (let match in matches) {
+        let matchObject = {
+            gpm: matches[match]["gold_per_min"],
+            xpm: matches[match]["xp_per_min"],
+            tower_damage: matches[match]["tower_damage"],
+            hero_damage: matches[match]["hero_damage"],
+            //kills: matches[match]["kills"],getdotastatistics?steamID=76561198404101751
+            //deaths: matches[match]["deaths"],
+            // assists: matches[match]["assists"],
+            denies: matches[match]["denies"],
+            kda: matches[match]["kda"],
+            last_hits: matches[match]["last_hits"],
+            start_time: matches[match]["start_time"],
+
+            //TODO: find a way to get win_lose history
+            win: openDotaStats.win_rate["win"],
+            lose: openDotaStats.win_rate["lose"],
+        };
+        
+        statistics.matches.push(matchObject);
+    }
+    
+    return statistics;
+}
+
 Routes.get('/dropstatistics', (req, res) => {
     FirbaseUtils.allUsersSteamIds().then((steamIds) => {
         for (let i = 0; i < steamIds.length; ++i) {
@@ -57,34 +87,7 @@ Routes.get('/fillstatistics', function (req, res) {
     if (req.query.steamID) {
       FirbaseUtils.getUserDotaStatisticsBySteamId(req.query.steamID).then((result) => 
       {
-          let statistics = {};
-
-          statistics.matches = [];
-
-          console.dir(result.value.matches);
-          for (let match in result.value.matches) {
-              let matchObject = {
-                  gpm: result.value.matches[match]["gold_per_min"],
-                  xpm: result.value.matches[match]["xp_per_min"],
-                  tower_damage: result.value.matches[match]["tower_damage"],
-                  hero_damage: result.value.matches[match]["hero_damage"],
-                  //kills: result.value.matches[match]["kills"],getdotastatistics?steamID=76561198404101751
-                  //deaths: result.value.matches[match]["deaths"],
-                 // assists: result.value.matches[match]["assists"],
-                  denies: result.value.matches[match]["denies"],
-                  kda: result.value.matches[match]["kda"],
-                  last_hits: result.value.matches[match]["last_hits"],
-                  start_time: result.value.matches[match]["start_time"],
-
-                  //TODO: find a way to get win_lose history
-                  win: result.value.win_rate["win"],
-                  lose: result.value.win_rate["lose"],
-                };
-
-              statistics.matches.push(matchObject);
-          }
-
-          res.send(statistics);
+          res.send(formatOpenDotaStatistics(result.value));
       });
     }
     else {
