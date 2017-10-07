c90a256c0c8545cbd69e652ff961139cf82ef336
Alexande Zolotov <slayer666@debian-slayer666.alexander.zolotov.net>
2017-10-07 21:46:44 +0300

    moved statistics fetching functionality to separate routes

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,11 @@ const Promise = require('bluebird')
 const OpenDotaUtils = require('./utils/opendota.js');
 const FirbaseUtils = require('./utils/firebase.js');
 
+const routes = require('./routes/routes.js');
+
+//  Connect all our routes to our application
+app.use('/', routes);
+
 app.use(cors({ credentials: true, origin: true }))
 app.use(bodyParser.json())
 
@@ -521,57 +526,6 @@ function (request, response) {
   } else {
     response.redirect('/?failed')
   }
-})
-
-app.get('/fillstatistics', function (req, res) {
-  let statNames = ["gold_per_min", "start_time", "hero_id", "xp_per_min", "last_hits", "tower_damage", "kills", "deaths", "assists", "denies", "kda"];
-
-  //fetch user steamID for each user from database
-  FirbaseUtils.allUsersSteamIds().then((steamIds) => {
-    for (let i = 0; i < steamIds.length; ++i) {
-    //get each user by for fetched steamId
-     FirbaseUtils.getUserBySteamId(steamIds[i]).then((user) => {
-       let userDotaId = user.value.dotaId;
-       
-       //win/lose
-       OpenDotaUtils.fetchWinLoseStatsForPlayerID(userDotaId).then(function (result) {
-        FirbaseUtils.addSingleChildToUser(user.key, "openDotaStatistics/win_rate", result);
-      });
-      
-      //mmr
-      OpenDotaUtils.fetchMMRStatsForPlayerID(userDotaId).then(function (result) {
-        FirbaseUtils.addSingleChildToUser(user.key, "openDotaStatistics/mmr", result);
-      });
-
-       //fetch num matches from statistics
-      OpenDotaUtils.howManyMatchesForPlayerID(userDotaId).then(function (amount) {
-        return amount;
-      }).then((numMatches) => {
-        //fetch num records from firebase
-        FirbaseUtils.howManyRecordsForUserSteamId(user.key, "openDotaStatistics/matches").then(function(numRecords) {
-          return numRecords; }).then(function(numRecords) {
-            //only fetch statistics, and update the firebase, if numMatches > numRecords
-          if (numMatches > numRecords) {
-            let limit = numMatches - numRecords;
-            OpenDotaUtils.fetchStatsForPlayerID(userDotaId, statNames, limit).then(function (stats) {
-              FirbaseUtils.addChildToUser(user.key, "openDotaStatistics/matches", stats);
-            })
-          }
-        });
-      });
-    });
-  }
-});
-
-res.send("testing methods...");
-})
-
-app.get('/dropstatistics', function (req, res) {
-  FirbaseUtils.allUsersSteamIds().then((steamIds) => {
-    for (let i = 0; i < steamIds.length; ++i) {
-      FirbaseUtils.addSingleChildToUser(steamIds[i], "openDotaStatistics", null);
-    }
-  }).then(() => {res.send("statistics dropped...")});
 })
 
 app.listen(3001, function () {
